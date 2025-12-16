3f503c5b3483c1898c3006e39a265bdf6616eeaa
Abbas Gholami <abbas.gholami71@gmail.com>
2025-12-16 15:19:53 +0330

    fix: update parser entry point and remove obsolete template tests

--- src/nomad_em/parsers/__init__.py
+++ src/nomad_em/parsers/__init__.py
@@ -1,9 +1,12 @@
 from nomad.config.models.plugins import ParserEntryPoint
 
+
 class SEMParserEntryPoint(ParserEntryPoint):
     def load(self):
         from nomad_em.parsers.parser import SEMParser
+
         return SEMParser(**self.dict())
+
 
 parser_entry_point = SEMParserEntryPoint(
     name='SEMParser',
