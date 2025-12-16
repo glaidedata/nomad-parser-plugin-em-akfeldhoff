758df942a1f6fe52a06ce9ef2b328dcbf7c6e6d0
Abbas Gholami <abbas.gholami71@gmail.com>
2025-12-16 11:45:04 +0330

    feat(schema): define SEMImage and SEMEntry structure

--- src/nomad_em/schema_packages/schema_package.py
+++ src/nomad_em/schema_packages/schema_package.py
@@ -1,19 +1,12 @@
-from typing import (
-    TYPE_CHECKING,
-)
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
-        BoundLogger,
-    )
+    pass
 
 from nomad.config import config
-from nomad.datamodel.data import Schema
+from nomad.datamodel.data import ArchiveSection, EntryData
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import Quantity, SchemaPackage
+from nomad.metainfo import Quantity, SchemaPackage, Section, SubSection
 
 configuration = config.get_plugin_entry_point(
     'nomad_em.schema_packages:schema_package_entry_point'
@@ -22,17 +15,65 @@ configuration = config.get_plugin_entry_point(
 m_package = SchemaPackage()
 
 
-class NewSchemaPackage(Schema):
-    name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
+class SEMImage(ArchiveSection):
+    """
+    Section representing a single SEM image and its extracted metadata.
+    """
+
+    image = Quantity(
+        type=str,
+        description='The image file (.bmp) associated with this metadata.',
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.FileEditQuantity, label='SEM Image'
+        ),
     )
-    message = Quantity(type=str)
 
-    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        super().normalize(archive, logger)
+    acceleration_voltage = Quantity(
+        type=float,
+        unit='kV',
+        description='Acceleration Voltage ($CM_ACCEL_VOLT)',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.NumberEditQuantity),
+    )
 
-        logger.info('NewSchema.normalize', parameter=configuration.parameter)
-        self.message = f'Hello {self.name}!'
+    magnification = Quantity(
+        type=float,
+        description='Magnification ($CM_MAG)',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.NumberEditQuantity),
+    )
+
+    working_distance = Quantity(
+        type=float,
+        unit='mm',
+        description='Working Distance ($$SM_WD)',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.NumberEditQuantity),
+    )
+
+    date = Quantity(
+        type=str,
+        description='Date of acquisition ($CM_DATE)',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+    )
+
+
+class SEMEntry(EntryData):
+    """
+    Top-level entry for an SEM experiment containing multiple images.
+    """
+
+    m_def = Section(
+        a_eln=ELNAnnotation(
+            label='SEM Experiment (JEOL)',
+        )
+    )
+
+    description = Quantity(
+        type=str,
+        description='Description of this SEM collection.',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+    )
+
+    # This SubSection holds the list of images found in the folder
+    images = SubSection(section_def=SEMImage, repeats=True, label='Detected Images')
 
 
 m_package.__init_metainfo__()
